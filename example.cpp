c1a154a66f3cb7d1d8f4937ae31550c9a2429e6b
Nwutobo Samuel Ugochukwu <sammycageagle@gmail.com>
2021-08-18 08:55:17 +0100

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -33,7 +33,7 @@ int main()
     {
         auto currentTime = animation.currentTime();
         auto progress = animation.progressAt(currentTime);
-        std::cout << "At " << currentTime << 's' << " x is " << x.valueAt(progress) << " and y is " << y.valueAt(progress) << std::endl;
+        std::cout << "At " << currentTime << "s x is " << x.valueAt(progress) << " and y is " << y.valueAt(progress) << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(25));
     }
 
